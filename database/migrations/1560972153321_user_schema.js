699b6525ac8598141988c39051d87f1a6c0bc605
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-19 16:28:10 -0300

    setup user role and mysql

---
+++ database/migrations/1560972153321_user_schema.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class UserSchema extends Schema {
+  up() {
+    this.table("users", table => {
+      table.enu("role", ["admin", "client"]);
+    });
+  }
+
+  down() {
+    this.table("users", table => {
+      table.dropColumn("role");
+    });
+  }
+}
+
+module.exports = UserSchema;
