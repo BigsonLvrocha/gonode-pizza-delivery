f82eea7c6bcac6a89767fa50ae0e71b1db956b1a
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-28 20:05:03 -0300

    adds validation no duplicate in array

--- providers/CompoundValidationProvider.js
+++ providers/CompoundValidationProvider.js
@@ -39,13 +39,31 @@ class CompoundValidationProvider extends ServiceProvider {
     if (!Array.isArray(ids)) {
       return
     }
-    const uniqueIds = [...new Set(ids)]
+    const id = args[2] ? args[2] : false
+    const uniqueIds = [...new Set(ids.map(item => (id ? item[id] : item)))]
     const table = args[0]
     const idField = args[1] ? args[1] : 'id'
     const rows = await Database.table(table)
       .whereIn(idField, uniqueIds)
       .getCount()
     if (rows !== uniqueIds.length) {
+      throw message
+    }
+  }
+
+  async noDuplicates (data, field, message, args, get) {
+    const items = get(data, field)
+    if (!items) {
+      return
+    }
+    if (!Array.isArray(items)) {
+      return
+    }
+    const idField = args[0] ? args[0] : false
+    const uniqueItems = [
+      ...new Set(items.map(item => (idField ? item[idField] : item)))
+    ]
+    if (uniqueItems.length < items.length) {
       throw message
     }
   }
@@ -63,6 +81,11 @@ class CompoundValidationProvider extends ServiceProvider {
 
     Validator.extend('uniqueCompound', this._uniqueFn, 'Must be unique')
     Validator.extend('idsExists', this.idsExists, 'all items must exist')
+    Validator.extend(
+      'noDuplicates',
+      this.noDuplicates,
+      'There must be no duplicates in array'
+    )
   }
 }
 
